fb5bc5852571a6472ed33d924a6a0f43f04753f7
sarvesh-kale <sarveshkale7217@gmail.com>
2021-06-20 20:31:52 +0530

    decimalrtobinary.c prints binaries in reverse

--- decimaltobinary.c
+++ decimaltobinary.c
@@ -2,13 +2,17 @@
 #include<stdlib.h>
 int main(int argc , char **argv)
 {	
-	int n;
-	n=atoi(argv[1]);
+	int n,i=0;	
+	while(i<512){
+	n=i ;
 	while(n>0)
 	{		
 		printf("%d",n%2);
 		n=n/2 ;
 	}
 	printf("\n");
+	
+	i++;
+    }
 	return 0;
 }

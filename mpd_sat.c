b5fa94a410b51a1556a281c091fd656c4de25186
sarvesh-kale <sarveshkale7217@gmail.com>
2021-06-20 18:52:03 +0530

    able to get the clauses printed on screen

---
+++ mpd_sat.c
@@ -0,0 +1,50 @@
+//this file receives three command line arguments 
+
+//first is path to   
+//second is nvars 
+//third  is nclauses 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+int main(int argc ,char **argv)
+{	
+ 	FILE *fptr ;
+	char *line = NULL ;
+	size_t len =0 ;
+	ssize_t read ;
+	fptr = fopen(argv[1],"r");
+	int nvars = atoi(argv[2]),nclauses=atoi(argv[3]);
+	
+	struct boolean{
+		bool value ;
+
+	};
+	int i = 1; // just for looping 
+	while(i<=nvars){ // define variables here 
+		bool var[i] ;
+		i ++ ;
+	}
+
+	if(fptr == NULL ){
+		printf("failed to open the file !! \n");
+		exit(1);
+	}
+
+	printf("file opened successfully \n");	
+	
+	while((read = getline(&line , &len ,fptr)) != -1 )// 
+	{
+		if(line[0] == 'c' || line[0] == 'p') // imagine line is pointing to string 	
+			continue ;                   // this skips line starting with c which are comments  
+		//len contains the length of line 
+		for(i=0 ; line[i] != '\0' ; i++) // loop condition is good 
+		{
+			
+		}
+				
+	}
+	return 0 ;
+}
+

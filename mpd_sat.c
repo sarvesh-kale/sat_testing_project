c98a4c292b7f6047ebdfe72b156fb856cd726551
sarvesh-kale <sarveshkale7217@gmail.com>
2021-06-20 23:09:52 +0530

    the output is getting printed for 2**n-1 linesbut it is wrong

--- mpd_sat.c
+++ mpd_sat.c
@@ -27,64 +27,87 @@ int main(int argc ,char **argv)
 	size_t len1 =0 ;
 	ssize_t read ;
 	ssize_t read1 ;
-	fptr = fopen(argv[1],"r");
+
 	fptr2= fopen("binaries512.txt","r");
-	int nvars = atoi(argv[2]),nclauses=atoi(argv[3]);
+	int nvars    = atoi(argv[2]), // no of variables 
+	    nclauses = atoi(argv[3]); // no of clauses 
 	bool cnf_expr=true;
 	bool clause=false;
 	int i = 1,n ; // just for looping 
 	int max=1;
 	// define cnf  variables here 
 	bool var[nvars+1] ;
-	while(i<=nvars)
+	while(i<=nvars){
 		max=max*2;
-	if(fptr == NULL ){
-		printf("failed to open the file !! \n");
-		exit(1);
+		i++ ;
 	}
 	if(fptr2 == NULL){
 		printf("Failed to open the file !! \n");
 	}
-	//printf("file opened successfully \n");	
+	printf("file opened successfully \n");	
 	
 	//i i loop variable  
 	n = 512 ; // 2**n ,max 9 variable possible 
 	for(int k=1;k<n && k<max;k++)
 	{
+		//initialize veryone to false ,those get overridden 
+		//
+		int counter = 1;
+		while(counter <= nvars){
+			var[counter] = false ;
+			counter++ ;
+		}
 		while((	read = getline(&line1,&len1,fptr2)) != -1)
 			for(int j=0;line1[j]!='\0';j++)
 			{
 				if(line1[j] == '1')
 					var[j+1]=true;
-				else 
+				else if (line1[j] == '0') 
 					var[j+1]=false;
+				//printf("here\n");
 			}
-		
-			
+				
+		fptr = fopen(argv[1],"r");
+		if(fptr == NULL ){
+			printf("failed to open the file !! \n");
+			exit(1);
+		}
+		cnf_expr=true ;
 		while((read = getline(&line , &len ,fptr)) != -1 )// 
 		{
 			if(line[0] == 'c' || line[0] == 'p') // imagine line is pointing to string 	
 				continue ;                   // this skips line starting with c which are comments  
 			//len contains the length of line 
 			clause = false ;// initialize to false 
-			for(i=0 ; line[i] != '\0' ; i++) // loop condition is good 
+			for(i=0 ; line[i] != '\0' ; ) // loop condition is good 
 			{
 				//we are going to construct the cnf first 
 				
-				if(line[i] == '-')
+				if(line[i] == '-'){
 					clause = or(not(var[(int)(line[i+1] - '0')]),clause);
-				else if((int)in(line[i])<=9)
+					i = i + 2;
+				}
+				else if((int)in(line[i])<=9 && line[i]!=' '){
 					clause = or(var[(int)(line[i] - '0')],clause) ;
-				else if(line[0] == '\0')
+					i = i + 1 ;
+				}
+				else if(line[i] == '0' || line[i] == ' '){
+					i = i + 1 ;
 					continue ;
+				}
 				
 			}
 			cnf_expr = and(clause,cnf_expr);
-				
+			//printf("I am here");	
 		}
+		if(cnf_expr == true)
+			printf("Sat\n");
+		else 
+			printf("Unsat\n");	
+		
+		fclose(fptr);
 	}
-	if(cnf_expr == true)
-		printf("Sat");
+	fclose(fptr2);
 	return 0 ;
 }
 
